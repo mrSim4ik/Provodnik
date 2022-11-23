e7e2f4c05d797b029b94f86399d5b985033c5536
mrSim4ik <isip_d.a.alekseev@mpt.ru>
2022-11-24 00:52:38 +0300

    hard

---
+++ francyzskayaPracticheskaya/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_7
+{
+    public static class GetFiles
+    {
+        public static List<File1> Get(string path)
+        {
+            List<File1> File1list = new List<File1>();
+            foreach (string file in Directory.GetDirectories(path))
+                File1list.Add(new File1(file.Split("\\")[^1]+"\\", file));
+            foreach (string file in Directory.GetFiles(path))
+                File1list.Add(new File1(file.Split("\\")[^1], file));
+            return File1list;
+        }
+    }
+    public class Cursor
+    {
+        public Cursor(int max = 1, int min = 1, int pos = 1)
+        {
+            this.max = max;
+            this.min = min; 
+            this.pos = pos;
+        }
+        public int max, min, pos;
+    }
+    public static class ShowInformation
+    {
+        public static void ShowDirs(List<File1> papkaList)
+        {
+            foreach (File1 elem in papkaList)
+                Console.WriteLine("   " + elem.name);
+        }
+    }
+    public class File1
+    {
+        public File1(string name, string path)
+        {
+            
+            this.path = path;
+            this.name = name;
+        }
+        public string name;
+        public string path;
+    }
+}
---
+++ francyzskayaPracticheskaya/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+
+namespace Практическая_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "\\";
+            Cursor cursor = new Cursor();
+            List<File1> filelist = new List<File1>();
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                cursor.max = 0;
+                Console.WriteLine("Проводник.ехе. Текущий путь - "+file);
+                if (file == "\\")
+                {
+                    filelist = new List<File1>();
+                    DriveInfo[] allDrives = DriveInfo.GetDrives();
+                    foreach (DriveInfo drive in allDrives)
+                    {
+                        double totalSpace = drive.TotalSize;
+                        double availableSpace = drive.AvailableFreeSpace;
+                        File1 elem = new File1($"Диск {drive.Name} | Размер {totalSpace} гб | Доступно {availableSpace} гб", drive.Name);
+                        filelist.Add(elem);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        filelist = GetFiles.Get(file);
+                    }
+                    catch
+                    {
+                        
+                    }
+                    
+                }
+                ShowInformation.ShowDirs(filelist);
+                cursor.max = filelist.Count;
+                Console.SetCursorPosition(0, cursor.pos);
+                Console.Write(">>");
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (Directory.Exists(filelist[cursor.pos - cursor.min].path))
+                        {
+                            file = filelist[cursor.pos - cursor.min].path;
+                        }
+                        else
+                        {
+                            Process.Start(filelist[cursor.pos - cursor.min].path);
+                        }
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (cursor.pos == cursor.max)
+                            cursor.pos = cursor.min;
+                        else
+                            cursor.pos++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (cursor.pos == cursor.min)
+                            cursor.pos = cursor.max;
+                        else
+                            cursor.pos--;
+                        break;
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
